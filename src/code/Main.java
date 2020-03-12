2a772444669e8056f3fcdf742db409edd41c6c7e
cyhaz <cyhaz@naver.com>
2020-03-13 01:16:28 +0900

    Add_MathQuiz
    - Math.pow() 와 Math.sqrt() 로 좌표간의 직선거리 구하기
    - 내풀이 / 강사님풀이
    - 출력값 저장

--- src/code/Main.java
+++ src/code/Main.java
@@ -22,6 +22,28 @@ public class Main {
 		// 루트7의 값을 소수점 2자리까지 출력
 		System.out.println(String.format("루트%d 값은 %.2f입니다.", sqrtNum, sqrtResult));
 		// 출력 : 루트7 값은 2.65입니다.
+		
+		
+//		< 응용문제. 좌표간의 직선거리 구하기 >
+//		* (x1, y1)과 (x2, y2)의 직선거리 = 루트((x2-x1)^2 + (y2-y1)^2) *
+//		문제. (1, 10)과 (13, 22)의 직선거리는?
+		
+		// 내 풀이
+		int x1 = 1;
+		int y1 = 10;
+		int x2 = 13;
+		int y2 = 22;
+		double distance1 = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
+		System.out.println(String.format("두 좌표간의 직선거리는 %.2f입니다.", distance1));
+		// 출력 : 두 좌표간의 직선거리는 16.97입니다.
+		
+		// 강사님 풀이
+		int xInterval = 13-1;
+		int yInterval = 22-10;
+		double distance2 = Math.sqrt((xInterval*xInterval) + (yInterval*yInterval));
+		System.out.println(String.format("두 좌표간의 직선거리는 %.2f입니다.", distance2));
+		// 출력 : 두 좌표간의 직선거리는 16.97입니다.		
+		
 	}
 
 }
